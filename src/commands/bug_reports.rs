0f63dad52eab8c2e6fa56526706387a23cf75dba
Aldan Tanneo <aldantanneo@gmail.com>
2021-05-11 17:44:26 +0200

    eat this

--- src/commands/bug_reports.rs
+++ src/commands/bug_reports.rs
@@ -268,8 +268,9 @@ pub async fn bug(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult 
                                     ""
                                 }
                             ));
-                            if let Ok(message) = linked_message {
-                                e.description(format!("{}\n[message link]({})", &message.content, &message.link()));
+                            if let Ok(mut message) = linked_message {
+                                message.guild_id = msg.guild_id;
+                                e.description(format!("{}\n[[message link]]({})", &message.content, &message.link()));
                                 if let Some(image) = message.attachments.get(0) {
                                     e.image(&image.url);
                                 }
