3a75f94ea7a69b5a782e2288f1cc4671fb8be4fd
Aldan Tanneo <aldantanneo@gmail.com>
2021-11-02 15:30:42 +0100

    q&a fixes

--- src/qa_answers.rs
+++ src/qa_answers.rs
@@ -4,6 +4,23 @@ use serenity::prelude::*;
 
 use crate::database::qa_data::*;
 
+fn extract_image_attachment(msg: &Message) -> Option<&Attachment> {
+    msg.attachments
+        .get(0)
+        .map(|a| {
+            if a.content_type
+                .as_ref()
+                .map(|s| s.starts_with("image"))
+                .unwrap_or_default()
+            {
+                Some(a)
+            } else {
+                None
+            }
+        })
+        .flatten()
+}
+
 pub async fn handle_reaction(ctx: &Context, reaction: Reaction, guild_id: GuildId) {
     if !is_questions_channel(ctx, guild_id, reaction.channel_id)
         .await
@@ -25,20 +42,33 @@ pub async fn handle_reaction(ctx: &Context, reaction: Reaction, guild_id: GuildI
     }
 
     if let Some(answers_channel) = get_answer_channel(ctx, guild_id).await {
-        if let Ok(answer) = reaction.message(ctx).await {
-            if let Some(question) = answer.referenced_message {
+        if let Ok(ref answer) = reaction.message(ctx).await {
+            if answer.author.id != user_id
+                || !answer
+                    .reactions
+                    .iter()
+                    .any(|react| react.me && react.reaction_type.unicode_eq("❓"))
+            {
+                return;
+            } else if let Err(e) = answer.delete_reaction_emoji(ctx, '❓').await {
+                println!("Error deleting '❓' reaction: {}", e);
+            }
+
+            if let Some(ref question) = answer.referenced_message {
+                let answer_attachment = extract_image_attachment(answer);
+                let question_attachment = extract_image_attachment(question);
+
                 answers_channel
                     .send_message(ctx, |m| {
+                        if let Some(attachment) = question_attachment {
+                            m.add_file(attachment.url.as_str());
+                        }
+                        if let Some(attachment) = answer_attachment {
+                            m.add_file(attachment.url.as_str());
+                        }
                         m.embed(|e| {
-                            if let Some(attachment) = question.attachments.get(0) {
-                                if attachment
-                                    .content_type
-                                    .as_ref()
-                                    .map(|s| s.starts_with("image"))
-                                    .unwrap_or_default()
-                                {
-                                    e.thumbnail(&attachment.url);
-                                }
+                            if let Some(attachment) = question_attachment {
+                                e.thumbnail(format!("attachment://{}", attachment.filename));
                             }
                             e.fields([
                                 (
@@ -56,15 +86,8 @@ pub async fn handle_reaction(ctx: &Context, reaction: Reaction, guild_id: GuildI
                                 a.name("LOTR Mod Q&A").icon_url(crate::constants::BOT_ICON)
                             });
                             e.colour(0xc27c0e);
-                            if let Some(attachment) = answer.attachments.get(0) {
-                                if attachment
-                                    .content_type
-                                    .as_ref()
-                                    .map(|s| s.starts_with("image"))
-                                    .unwrap_or_default()
-                                {
-                                    e.image(&attachment.url);
-                                }
+                            if let Some(attachment) = answer_attachment {
+                                e.attachment(&attachment.filename);
                             }
                             e.footer(|f| f.icon_url(answer.author.face()));
                             e.timestamp(&answer.timestamp);
