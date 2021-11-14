e1a31a23089add0e1a3aa58fd1c053e5fd907d72
Aldan Tanneo <aldantanneo@gmail.com>
2021-11-15 00:32:32 +0100

    fix too long message issue in q&a

--- src/qa_answers.rs
+++ src/qa_answers.rs
@@ -41,69 +41,95 @@ pub async fn handle_reaction(ctx: &Context, reaction: Reaction, guild_id: GuildI
         return;
     }
 
-    if let Some(answers_channel) = get_answer_channel(ctx, guild_id).await {
-        if let Ok(ref answer) = reaction.message(ctx).await {
-            if answer.author.id != user_id
-                || !answer
-                    .reactions
-                    .iter()
-                    .any(|react| react.me && react.reaction_type.unicode_eq("❓"))
-            {
-                return;
-            } else if let Err(e) = answer.delete_reaction_emoji(ctx, '❓').await {
-                println!("Error deleting '❓' reaction: {}", e);
-            }
-
-            if let Some(ref question) = answer.referenced_message {
-                let answer_attachment = extract_image_attachment(answer);
-                let question_attachment = extract_image_attachment(question);
-
-                answers_channel
-                    .send_message(ctx, |m| {
-                        if let Some(attachment) = question_attachment {
-                            m.add_file(attachment.url.as_str());
-                        }
-                        if let Some(attachment) = answer_attachment {
-                            m.add_file(attachment.url.as_str());
-                        }
-                        m.embed(|e| {
-                            if let Some(attachment) = question_attachment {
-                                e.thumbnail(format!("attachment://{}", attachment.filename));
-                            }
-                            e.fields([
-                                (
-                                    format!("**Question by {}**", question.author.name),
-                                    &question.content,
-                                    false,
-                                ),
-                                (
-                                    format!("**Answer by {}**", answer.author.name),
-                                    &answer.content,
-                                    false,
-                                ),
-                            ]);
-                            e.author(|a| {
-                                a.name("LOTR Mod Q&A").icon_url(crate::constants::BOT_ICON)
-                            });
-                            e.colour(0xc27c0e);
-                            if let Some(attachment) = answer_attachment {
-                                e.attachment(&attachment.filename);
-                            }
-                            e.footer(|f| f.icon_url(answer.author.face()));
-                            e.timestamp(&answer.timestamp);
-                            e
-                        })
-                    })
-                    .await
-                    .expect("Could not send answer message in answers channel");
-            } else {
-                println!("Answer has no referenced question message");
-            }
-        } else {
-            println!("Could not get answer message from its id");
-        }
+    let answers_channel = if let Some(answers_channel) = get_answer_channel(ctx, guild_id).await {
+        answers_channel
     } else {
         println!("Could not find answer channel for this server");
+        return;
+    };
+
+    let answer = if let Ok(answer) = reaction.message(ctx).await {
+        answer
+    } else {
+        println!("Could not get answer message from its id");
+        return;
+    };
+
+    if answer.author.id != user_id
+        || !answer
+            .reactions
+            .iter()
+            .any(|react| react.me && react.reaction_type.unicode_eq("❓"))
+    {
+        return;
+    } else if let Err(e) = answer.delete_reaction_emoji(ctx, '❓').await {
+        println!("Error deleting '❓' reaction: {}", e);
+    }
+
+    let question = if let Some(ref question) = answer.referenced_message {
+        question
+    } else {
+        println!("Answer has no referenced question message");
+        return;
+    };
+
+    let answer_attachment = extract_image_attachment(&answer);
+    let question_attachment = extract_image_attachment(question);
+
+    if let Err(e) = answers_channel
+        .send_message(ctx, |m| {
+            if let Some(attachment) = question_attachment {
+                m.add_file(attachment.url.as_str());
+            }
+            if let Some(attachment) = answer_attachment {
+                m.add_file(attachment.url.as_str());
+            }
+            m.embed(|e| {
+                if let Some(attachment) = question_attachment {
+                    e.thumbnail(format!("attachment://{}", attachment.filename));
+                }
+                if answer.content.len() <= 1024 && question.content.len() <= 1024 {
+                    e.fields([
+                        (
+                            format!("**Question by {}**", question.author.name),
+                            &question.content,
+                            false,
+                        ),
+                        (
+                            format!("**Answer by {}**", answer.author.name),
+                            &answer.content,
+                            false,
+                        ),
+                    ]);
+                } else {
+                    e.description(format!(
+                        "**Question by {question_author}**
+{question_content}
+
+**Answer by {answer_author}**
+{answer_content}",
+                        question_author = question.author.name,
+                        question_content = question.content,
+                        answer_author = answer.author.name,
+                        answer_content = answer.content
+                    ));
+                }
+                e.author(|a| a.name("LOTR Mod Q&A").icon_url(crate::constants::BOT_ICON));
+                e.colour(0xc27c0e);
+                if let Some(attachment) = answer_attachment {
+                    e.attachment(&attachment.filename);
+                }
+                e.footer(|f| f.icon_url(answer.author.face()));
+                e.timestamp(&answer.timestamp);
+                e
+            })
+        })
+        .await
+    {
+        println!(
+            "=== Q&A ERROR ===\nError sending Q&A answer: {}\n=== END ===",
+            e
+        );
     }
 }
 
